f74f273b68c62165729d6363aa9ef8ad0f397ce3
Elise Kuylen <elisekuylen@outlook.com>
2017-04-19 10:11:02 +0200

    Added data class for Health Belief Model.

---
+++ src/main/cpp/behaviour/belief_data/HBMData.h
@@ -0,0 +1,21 @@
+/*
+ * HBMData.h
+ *
+ *  Created on: Apr 19, 2017
+ *      Author: elise
+ */
+
+#ifndef SRC_MAIN_CPP_BEHAVIOUR_BELIEF_DATA_HBMDATA_H_
+#define SRC_MAIN_CPP_BEHAVIOUR_BELIEF_DATA_HBMDATA_H_
+
+
+class HBMData {
+private:
+	double m_perceived_severity;
+	double m_perceived_susceptibility;
+	double m_perceived_benefit;
+	double m_perceived_barriers;
+};
+
+
+#endif /* SRC_MAIN_CPP_BEHAVIOUR_BELIEF_DATA_HBMDATA_H_ */
