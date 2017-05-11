dda86bf06bc04ee95af8db04315cc7273be00561
Elise Kuylen <elisekuylen@outlook.com>
2017-05-11 23:03:17 +0200

    Added classes for Global and Local information policies.

---
+++ src/main/cpp/behaviour/information_policies/LocalInformationPolicy.h
@@ -0,0 +1,20 @@
+/*
+ * LocalInformationPolicy.h
+ *
+ *  Created on: May 11, 2017
+ *      Author: elise
+ */
+
+#ifndef SRC_MAIN_CPP_BEHAVIOUR_INFORMATION_POLICIES_LOCALINFORMATIONPOLICY_H_
+#define SRC_MAIN_CPP_BEHAVIOUR_INFORMATION_POLICIES_LOCALINFORMATIONPOLICY_H_
+
+namespace stride {
+
+templace <bool prevalence_based, bool belief_based>
+class LocalInformationPolicy {
+
+};
+
+} /* namespace stride */
+
+#endif /* SRC_MAIN_CPP_BEHAVIOUR_INFORMATION_POLICIES_LOCALINFORMATIONPOLICY_H_ */
