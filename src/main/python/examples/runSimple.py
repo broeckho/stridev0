3abceaac650bf9e5d3dd2774b8c88effa4be34d8
Elise Kuylen <elise.kuylen@uantwerpen.be>
2017-11-23 14:28:18 +0100

    Link data dirs in workspace.

--- src/main/python/examples/runSimple.py
+++ src/main/python/examples/runSimple.py
@@ -2,6 +2,8 @@ from pystride.Simulation import Simulation
 
 # Load configuration from file
 simulation = Simulation()
-simulation.loadRunConfig("config/run_python.xml")
+simulation.loadRunConfig("config/run_default.xml")
+# Print the loaded configuration
+simulation.showRunConfig()
 # Run simulation
 simulation.run()
--- src/main/python/pystride/Simulation.py
+++ src/main/python/pystride/Simulation.py
@@ -10,54 +10,70 @@ class Simulation():
     def __init__(self):
         self.forks = list()
         self.simulator = None
-        # TODO observer
-        self.label = "Default"
-        self.configFile = 'pystride/default_configs/run_default.xml'
-        self._runConfig = None      # ETree with run config for simulation
-        self._diseaseConfig = None  # ETree with disease config
-        self._setDefaultConfig()    # Set default values for run & disease config
-
-    def _setDefaultConfig(self):
-        # Load run config
-        self._runConfig = ET.parse('pystride/default_configs/run_default.xml').getroot()
-        new_label = self.getRunConfigParam('output_prefix')
-        if new_label != None:
-            self.label = new_label
-        # Load disease config
-        disease_file = self.getRunConfigParam('disease_config_file')
-        self._diseaseConfig = ET.parse(os.path.join('pystride/default_configs', disease_file)).getroot()
+        #TODO observer
+        self._runConfig = None              # ElementTree with run config
+        self._diseaseConfig = None          # ElementTree with disease config
+        self.label = "default"
 
     def loadRunConfig(self, filename: str):
-        """ Load a configuration from a file """
-        old_disease_file = self.getRunConfigParam('disease_config_file')
-        self._config = ET.parse(filename).getroot()
-        if new_label != None:
-            self.label = new_label
-        new_disease_file = self.getRunConfigParam('disease_config_file')
-        if new_disease_file != old_disease_file:
-            self._diseaseConfig = ET.parse(new_disease_file).getroot()
+        runConfigFile = os.path.join(self.getWorkingDirectory(), filename)
+        if os.path.isfile(runConfigFile):
+            self._runConfig = ET.parse(runConfigFile).getroot()
+            # Update disease configuration
+            self._updateDiseaseConfig()
+            # Update label
+            if self._runConfig.find("output_prefix").text:
+                self.label = self._runConfig.find("output_prefix").text
+        else:
+            print("Run configuration file " + filename + " could not be found")
+
+    def _updateDiseaseConfig(self):
+        diseaseFilename = self._runConfig.find("disease_config_file").text
+        diseaseFile = os.path.join(self.getWorkingDirectory(), diseaseFilename)
+        if os.path.isfile(diseaseFile):
+            self._diseaseConfig = ET.parse(diseaseFile).getroot()
+        else:
+            print("Disease configuration file " + diseaseFilename + " could not be found")
 
     def getRunConfigParam(self, name: str):
-        if self._runConfig != None:
-            configParam = self._runConfig.find(name).text
-            if configParam != None:
-                return configParam
-            else:
-                print("No configuration parameter with name " + name + " could be found")
-        else:
-            print("No run configuration found")
+        if self._runConfig:
+            if self._runConfig.find(name).text:
+                return self._runConfig.find(name).text
+        print("No run configuration parameter with namme " + name)
 
-    def setRunConfigParam(self, name:str, value):
-        '''configParam = self._config.find(name)
-        if configParam != None:
-            configParam.text = str(value)
+    def getDiseaseConfigParam(self, name: str):
+        # TODO
+        pass
+
+    def setRunConfigParam(self, name: str, value):
+        if _runConfig:
+            if self._runConfig.find(name):
+                self._runConfig.find(name).text = str(value)
+            else:
+                ET.SubElement(self._runConfig, name).text = str(value)
         else:
-            print("No configuration parameter with name {} could be found", name)'''
+            self._runConfig = ET.Element('run')
+            ET.SubElement(self._runConfig, name).text = str(value)
+
+    def setDiseaseConfigParam(self, name: str, value):
+        # TODO
         pass
 
     def showRunConfig(self):
-        print("Run configuration:")
-        print(ET.tostring(self._runConfig))
+        """ Print out the run configuration parameters. """
+        if self._runConfig:
+            print("Run configuration: ")
+            print(ET.tostring(self._runConfig))
+        else:
+            print("No run configuration found!")
+
+    def showDiseaseConfig(self):
+        """ Print out the disease configuration parameters specified in the disease file. """
+        if self._diseaseConfig:
+            print("Disease configuration: ")
+            print(ET.tostring(self._diseaseConfig))
+        else:
+            print("No disease configuration found!")
 
     def stop(self):
         """ Stop the simulation if it's running """
@@ -88,34 +104,80 @@ class Simulation():
         return pystride.workspace
 
     def getOutputDirectory(self):
-        pass
+        return os.path.join(self.getWorkingDirectory(), self.label)
 
     def _linkData(self):
-        pass
+        dataDir = os.path.join(self.getOutputDirectory(), "data")
+        os.makedirs(dataDir, exist_ok=True)
+        files = [
+            self.getRunConfigParam('population_file'),
+            self.getRunConfigParam('disease_config_file'),
+            self.getRunConfigParam('holidays_file'),
+            self.getRunConfigParam('age_contact_matrix_file'),
+        ]
+        for src in files:
+            dst = os.path.join(dataDir, os.path.basename(src))
+            if os.path.isfile(src) and not os.path.isfile(dst):
+                os.symlink(src, dst)
 
     def _setup(self, linkData=True):
         """
             Create folder in workspace to run simulation.
             Copy config and link to data.
         """
-        pass
+        if linkData:
+            self._linkData()
+
+    '''
+            os.makedirs(self.getOutputDirectory(), exist_ok=True)
+            diseasePath = os.path.join(self.getOutputDirectory(), "data", self.disease.label + ".xml")
+            self.disease.toFile(diseasePath)
+
+            configPath = os.path.join(self.getOutputDirectory(), self.label + ".xml")
+            # only store last part of label (previous dirs already made)
+            oldLabel = self.label
+            self.label = os.path.basename(self.label)
+            self.toFile(configPath)
+            self.label = oldLabel
+    '''
 
     def _build(self, *args, **kwargs):
-        self.simulator = SimulationUtils.getSimulator(self._runConfig)
+        pass
+
+    '''
+        def build(self, runParallel=True, trackIndexCase=False, output=True):
+            self.simulator = getSimulator(self, self.getWorkingDirectory(), runParallel, trackIndexCase, output)
+            if self.simulator:
+                self.simulator.registerObserver(self.observer)
+    '''
 
     def run(self, *args, **kwargs):
         """ Run current simulation. """
-        # Check if setup is done and if necessary continue previous simulaitons.
+        # Check if setup is done and if necessary continue previous simulations.
         self._setup()
 
-        self._build(*args, *kwargs)
-        if self.simulator:
-            try:
-                numDays = self.getRunConfigParam("num_days")
-                self.simulator.Run(numDays)
-            except:
-                print("Exception when running simulator. Closing down.")
-                exit(1)
+    '''
+
+        def run(self, *args, numDays=0, **kwargs):
+            """ Run current simulation.
+
+            Check if setup is done and if necessary continue previous simulations. """
+            self.setup()
+
+            if len(self.getUQProperties()) > 0:
+                print("Found PUQ parameters, running PUQ")
+                self.runPUQ(*args, numDays=numDays, **kwargs)
+                return
+
+            self.build(*args, **kwargs)
+
+            if self.simulator:
+                try:
+                    self.simulator.Run(numDays)
+                except:
+                    print("Exception when running simulator. Closing down.")
+                    exit(0)
+    '''
 
     def runForks(self, *args, **kwargs):
         """ Run all forks but not the root simulation. """
@@ -133,7 +195,6 @@ class Simulation():
 
     def __setstate__(self, state):
         pass
-
 
 from .Fork import Fork
 
